424947c872f8f712e031c92660e5a444316d9260
I-am-Harshdeep <beyourselfharsh@gmail.com>
2020-01-25 10:29:23 +0530

    adding files

---
+++ script.js
@@ -0,0 +1,8 @@
+const progessBar = document.getElementsByClassName
+('progress-bar')[0]
+setInterval(()=> {
+    const computedStyle = getComputedStyle(progessBar)
+    const width = parseFloat(computedStyle.getPropertyValue
+        ('--width')) || 0
+        progessBar.style.setProperty('--width', width + .1)
+}, 1)
